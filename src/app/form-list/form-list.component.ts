3469821479c30ba8c4dbdefd69f1520d945f8d39
f.barzegar <fmuser1999@gmail.com>
2022-09-12 21:22:45 -0700

    Merge remote-tracking branch 'github/master'

--- src/app/form-list/form-list.component.ts
+++ src/app/form-list/form-list.component.ts
@@ -17,38 +17,62 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 })
 export class FormListComponent implements OnInit {
   form!: FormGroup;
-  comment?: String;
 
+  restForm=new FormGroup({
+    message:new FormControl(''),
+    nationalCode:new FormControl('')
+  })
   firstName = new FormControl("", Validators.required);
+   private url:string='http://localhost:8080/api/v1/chapar/insecure'
+  // private shahkarUrl: string = "http://192.168.16.171:8080/shahkar-secondary-end-point";
+  //
+  //
+  // private fatemehShahkarReq: ShahkarReqJson = new ShahkarReqJson("0022236457", "09924011072");
 
-  private shahkarUrl: string = "http://192.168.16.171:8080/shahkar-secondary-end-point";
-  // model = new ChaparRQ("0034", "0035");
 
-  private fatemehShahkarReq: ShahkarReqJson = new ShahkarReqJson("0022236457", "09924011072");
-
-  // private dtaa!:FormControl;
 
   ngOnInit(): void {
 
-    this.showComment();
-    // this.onSubmit(this.dtaa);
-    //
+    // this.showComment();
+
   }
 
-  constructor(private http: HttpClient,
-              private smsServiceService: SMSServiceService
+  constructor(private http: HttpClient, private smsServiceService: SMSServiceService
     , fb: FormBuilder) {
-    this.form = fb.group({
-      "firstName": this.firstName,
-      "password": ["", Validators.required]
-    });
+    // this.form = fb.group({
+    //   "firstName": this.firstName,
+    //   "password": ["", Validators.required]
+    // });
   }
 
-  private showComment(): void {
-    this.smsServiceService.getResponse(this.fatemehShahkarReq)
-      .subscribe(value => this.comment = value)
+  // private showComment(): void {
+  //   this.smsServiceService.getResponse(this.fatemehShahkarReq)
+  //     .subscribe(value => this.comment = value)
+  //
+  // }
 
+
+
+
+  // @ts-ignore
+  // onSubmit(mm) {
+  //   mm = this.form.value
+  //   console.log(mm)
+  //   this.smsServiceService.getResponse(mm)
+  //     .subscribe((result) => {
+  //       console.warn("result", result);
+  //     })
+  //
+  // }
+
+  onSubmit(){
+    // console.warn(this.restForm.value)
+
+    this.http.post(this.url,this.restForm.value).subscribe((data)=>{
+      console.warn(data)
+    })
   }
+
 
   //
   // @ts-ignore
@@ -60,16 +84,5 @@ export class FormListComponent implements OnInit {
   //     })
   // }
   // console.warn(data);
-
-
-  onSubmit(mm) {
-    mm = this.form.value
-    console.log(mm)
-    this.smsServiceService.getResponse(mm)
-      .subscribe((result) => {
-        console.warn("result", result);
-      })
-
-  }
 
 }
