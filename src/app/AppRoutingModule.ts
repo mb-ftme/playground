d636912d9117c6599d6c492d2c10a3a3c1022d0f
f.barzegar <fmuser1999@gmail.com>
2022-09-13 04:06:45 -0700

    Merge remote-tracking branch 'github/master'

---
+++ src/app/AppRoutingModule.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponentComponent} from "./login-component/login-component.component";
+import {FormListComponent} from "./form-list/form-list.component";
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponentComponent },
+  { path: 'form', component: FormListComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
